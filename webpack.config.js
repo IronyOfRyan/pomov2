3858a59e0b67dd8a40e835173492b5a2e3e8809c
RJ <oshmagoshzz@gmail.com>
2018-05-08 21:35:34 -0400

    config change

--- webpack.config.js
+++ webpack.config.js
@@ -38,11 +38,6 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.optimize.AggressiveMergingPlugin(),
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("production")
-      }
-    })
+    new webpack.optimize.AggressiveMergingPlugin()
   ]
 };
