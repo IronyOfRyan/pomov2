fb901ecdf6ef6a5f0179ecacf207cd9b7f93a1de
Ryan Paige <oshmagoshzz@gmail.com>
2021-03-18 22:57:41 -0400

    todos added

--- src/component/App.js
+++ src/component/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Timer from "./timer.js";
 import AppContent from "../styles/appcontent.js";
-
+// todos? overlay fix, color fixes, mordernize, change icons, style changes, smaller layout
 const App = () => {
   return (
     <AppContent>
